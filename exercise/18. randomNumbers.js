e4e97b6f40e94245977a2613dca0fc7236e1387c
Kapustin Stepan <s.kapustin@webpractik.ru>
2023-08-07 16:15:51 +0300

    commit last change

--- exercise/18. randomNumbers.js
+++ exercise/18. randomNumbers.js
@@ -2,6 +2,6 @@ import _ from 'lodash';
 
 const numbers = _.random(0, 100);
 
-const floatNumbers = _.random(0, 1, 0.1).toFixed(2);
+const floatNumbers = _.random(true).toFixed(2);
 
 console.log(numbers, floatNumbers);
